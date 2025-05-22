c0720cc27cdc963ff96a1b1f7a47614bbaa996fc
Naitik <134581392+Naitik-98@users.noreply.github.com>
2025-05-22 23:30:26 +0600

    dashboard modify

--- Level Up/Forms/DashboardForm.Designer.cs
+++ Level Up/Forms/DashboardForm.Designer.cs
@@ -28,7 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(299, 669);
+            this.panel1.TabIndex = 0;
             // 
             // DashboardForm
             // 
@@ -36,6 +44,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1384, 681);
+            this.Controls.Add(this.panel1);
             this.Name = "DashboardForm";
             this.Text = "DashboardForm";
             this.ResumeLayout(false);
@@ -43,5 +52,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }

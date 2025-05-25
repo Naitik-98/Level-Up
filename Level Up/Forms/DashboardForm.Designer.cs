e109c1cd3c3e8150febfc00609c4c72a06c99d2a
Romman-Tonmoy <rommontonmoy@gmail.com>
2025-05-26 00:06:31 +0600

    dash board

--- Level Up/Forms/DashboardForm.Designer.cs
+++ Level Up/Forms/DashboardForm.Designer.cs
@@ -28,16 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelTop = new System.Windows.Forms.Label();
+            this.labelMain = new System.Windows.Forms.Label();
+            this.btnAccount = new System.Windows.Forms.Button();
+            this.btnLibrary = new System.Windows.Forms.Button();
+            this.btnStore = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.panelLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // panelLeft
             // 
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(299, 669);
-            this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panelLeft.Controls.Add(this.btnSettings);
+            this.panelLeft.Controls.Add(this.btnStore);
+            this.panelLeft.Controls.Add(this.btnLibrary);
+            this.panelLeft.Controls.Add(this.btnAccount);
+            this.panelLeft.Controls.Add(this.pictureBox1);
+            this.panelLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(300, 700);
+            this.panelLeft.TabIndex = 0;
+            this.panelLeft.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Level_Up.Properties.Resources.Picsart_25_05_21_17_53_38_533;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 284);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // labelTop
+            // 
+            this.labelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(32)))));
+            this.labelTop.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTop.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelTop.Location = new System.Drawing.Point(297, 0);
+            this.labelTop.Name = "labelTop";
+            this.labelTop.Size = new System.Drawing.Size(1090, 81);
+            this.labelTop.TabIndex = 2;
+            this.labelTop.Text = "Level Up";
+            this.labelTop.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelMain
+            // 
+            this.labelMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(32)))));
+            this.labelMain.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelMain.Location = new System.Drawing.Point(298, 81);
+            this.labelMain.Name = "labelMain";
+            this.labelMain.Size = new System.Drawing.Size(1089, 591);
+            this.labelMain.TabIndex = 3;
+            this.labelMain.Text = "Main label";
+            // 
+            // btnAccount
+            // 
+            this.btnAccount.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnAccount.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAccount.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAccount.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAccount.Location = new System.Drawing.Point(53, 311);
+            this.btnAccount.Name = "btnAccount";
+            this.btnAccount.Size = new System.Drawing.Size(194, 56);
+            this.btnAccount.TabIndex = 2;
+            this.btnAccount.Text = "ACCOUNT";
+            this.btnAccount.UseVisualStyleBackColor = false;
+            // 
+            // btnLibrary
+            // 
+            this.btnLibrary.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnLibrary.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLibrary.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLibrary.Location = new System.Drawing.Point(53, 403);
+            this.btnLibrary.Name = "btnLibrary";
+            this.btnLibrary.Size = new System.Drawing.Size(194, 56);
+            this.btnLibrary.TabIndex = 3;
+            this.btnLibrary.Text = "LIBRARY";
+            this.btnLibrary.UseVisualStyleBackColor = false;
+            // 
+            // btnStore
+            // 
+            this.btnStore.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnStore.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStore.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnStore.Location = new System.Drawing.Point(53, 493);
+            this.btnStore.Name = "btnStore";
+            this.btnStore.Size = new System.Drawing.Size(194, 56);
+            this.btnStore.TabIndex = 4;
+            this.btnStore.Text = "STORE";
+            this.btnStore.UseVisualStyleBackColor = false;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnSettings.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSettings.Location = new System.Drawing.Point(53, 580);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(194, 56);
+            this.btnSettings.TabIndex = 5;
+            this.btnSettings.Text = "SETTINGS";
+            this.btnSettings.UseVisualStyleBackColor = false;
             // 
             // DashboardForm
             // 
@@ -45,15 +150,29 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1384, 681);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelMain);
+            this.Controls.Add(this.labelTop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelLeft);
             this.Name = "DashboardForm";
             this.Text = "DashboardForm";
+            this.panelLeft.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelTop;
+        private System.Windows.Forms.Label labelMain;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnStore;
+        private System.Windows.Forms.Button btnLibrary;
+        private System.Windows.Forms.Button btnAccount;
     }
 }
